6606383e1fc6f0df26f95fc43f66646eb96b872d
Noah Vassards <noah.vassards@epitech.eu>
2024-10-19 11:30:35 +0200

    fix

--- webPage/back-end/src/modules/alarms/alarm.module.ts
+++ webPage/back-end/src/modules/alarms/alarm.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { alarmProviders } from './alarm.provider';
 import { AlarmsService } from './alarm.service';
 import { AlarmsController } from './alarm.controller';
+import { MqttModule } from '../mqtt/mqtt.module';
 
 @Module({
+    imports: [MqttModule],
     providers: [AlarmsService, ...alarmProviders, ],
     exports: [AlarmsService],
     controllers: [AlarmsController]
